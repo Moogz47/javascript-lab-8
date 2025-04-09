ba039964b56cbb986c9931c3a906be4608dfc66b
Julila Mpesha <julilampesha@gmail.com>
2025-04-09 18:11:09 -0400

    Console log for outputting name and age properties, as well as calling the method.

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ const student = {
       console.log(`Name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses.join(", ")}`);
     }
   };
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+
+student.displayInfo();
